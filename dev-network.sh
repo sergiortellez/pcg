5b13d906146aa79162a6509e3190d5c9a42d2895
sergiortellez <sergiortellez@gmail.com>
2025-06-21 19:15:20 -0600

    Initial commit

---
+++ dev-network.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+# Get the current IP address on macOS
+IP=$(ipconfig getifaddr "$(route get default | awk '/interface: / { print $2 }')")
+
+# Kill any process using Vite's default port (5173 for Vite)
+lsof -ti:5173 | xargs kill -9 2>/dev/null
+
+# Construct the full URL
+URL="http://$IP:5173"
+
+# Start the Vite app without opening a browser
+BROWSER=none npm run dev &
+
+# Wait for the server to start
+sleep 5
+
+# AppleScript to check if Google Chrome is running
+APPLESCRIPT_CHECK_APP="
+if application \"Google Chrome\" is running then
+    return true
+else
+    return false
+end if
+"
+
+# AppleScript to open the URL in an existing or new window
+APPLESCRIPT_OPEN_URL="
+tell application \"Google Chrome\"
+    if (count of windows) > 0 then
+        set theTab to make new tab at the end of tabs of front window
+        set URL of theTab to \"$URL\"
+    else
+        make new window
+        set URL of active tab of front window to \"$URL\"
+    end if
+    activate
+end tell
+"
+
+# Check if Google Chrome is running
+IS_RUNNING=$(osascript -e "$APPLESCRIPT_CHECK_APP")
+
+if [ "$IS_RUNNING" = "true" ]; then
+    # Open URL in existing Chrome window
+    osascript -e "$APPLESCRIPT_OPEN_URL"
+    
+    osascript -e '
+    tell application "Google Chrome"
+        activate
+        tell application "System Events"
+            keystroke "i" using {command down, option down}
+        end tell
+    end tell
+    '
+else
+    # Start Google Chrome and open the URL
+    open -a "Google Chrome" "$URL"
+fi
+
+# Open the URL in Safari
+osascript -e "
+tell application \"Safari\"
+    activate
+    tell window 1
+        set current tab to tab 1
+        set URL of current tab to \"$URL\"
+    end tell
+end tell
+"
+
+# Enter Responsive Design Mode in Safari
+osascript -e '
+tell application "Safari"
+    activate
+    tell application "System Events"
+        keystroke "r" using {control down, command down}
+    end tell
+end tell
+'
+
+# Copy the URL to the clipboard
+echo $URL | pbcopy
+
+echo "Development server running on network and copied to clipboard."
+
+
+#                  🆁🅴🅰🅳 🅼🅴
+
+# Don't forget to adjust the scripts in package.json accordingly:
+
+# "scripts": {
+#   "dev": "vite", // This is the default script for starting a Vite project
+#   "start-network": "./start-network.sh"
+# }
+
+# And the first time, you must run this command to give execution permissions:
+# chmod +x start-network.sh
