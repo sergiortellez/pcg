ae13024bcabf438d2ee5cc08014108e00b234eb7
sergiortellez <sergiortellez@gmail.com>
2025-06-23 22:42:47 -0600

    @project(all), added:     - New Fonts for all the project.

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+// Fonts
+import '@fontsource/libre-baskerville';
+import '@fontsource-variable/inter';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
